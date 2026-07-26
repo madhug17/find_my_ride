945472576f50869aaa16d0fae1508db4667aa635
G Madhu Goud <goundlamadhug@gmail.com>
2026-07-26 22:38:18 +0530

    added fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI,HTTPException
-app = FastAPI()
+from fastapi import FastAPI
+app =FastAPI(
+    title="Find My Ride API",
+    version="1.0.0"
+)
+@app.get('/')
+def root():
+    return{
+        "message": "|Welcome to Find My Ride API|"
+    }
 @app.get('/health')
 def health():
-    return{"fine"}
+    return{
+        "status": "Healthy"
+    }
