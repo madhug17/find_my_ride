c9e42e88f257ab994652bb656c188bee01471063
G Madhu Goud <199909478+madhug17@users.noreply.github.com>
2026-08-27 15:53:25 +0530

    updated  test in ci.yaml

--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -1,2 +1,6 @@
-def test_basic():
-    assert 1+1==2
+from fastapi.testclient import TestClient
+from app.main import app
+client = TestClient(app)
+def test_app_status():
+    response = client.get("/docs")
+    assert response.status_code==200
